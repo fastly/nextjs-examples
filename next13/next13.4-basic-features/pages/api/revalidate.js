71bac881fc7f7324ab74e32414da62b5cee9e301
Katsuyuki Omuro <komuro@fastly.com>
2023-06-19 11:05:59 +0900

    fix revalidate paths

--- next13/next13.4-basic-features/pages/api/revalidate.js
+++ next13/next13.4-basic-features/pages/api/revalidate.js
@@ -1,13 +1,21 @@
 export default async function handler(req, res) {
-  // Check for secret to confirm this is a valid request
+  // In a real app you would check for a secret to confirm this is a valid request
   // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
   //   return res.status(401).json({ message: 'Invalid token' });
   // }
 
+  const { slug } = req.query;
+
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/datafetching/paths3/foo');
+    await res.revalidate(`/datafetching/paths-blocking/${slug ?? 'foo'}`);
+    try {
+      await res.revalidate(`/datafetching/paths-nofallback/${slug ?? 'foo'}`);
+    } catch {
+      // it's ok to 404 on this
+    }
+    await res.revalidate(`/datafetching/paths-fallback/${slug ?? 'foo'}`);
     return res.json({ revalidated: true });
   } catch (err) {
     console.log(err);
